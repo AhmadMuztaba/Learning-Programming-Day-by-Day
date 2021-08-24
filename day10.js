3ffd5941d3d15ce8b6b3d2db11bb50f100911542
AhmadMuztaba <ahmadmuztabasazi@gmail.com>
2021-08-24 22:10:58 +0600

    day 10 complete

---
+++ day10.js
@@ -0,0 +1,18 @@
+const nums=[-1,2,-3]
+let max=nums[0];
+let num=0;
+const sum=(k,l,highest,total)=>{
+    if(l===nums.length){
+        return highest;
+    }
+    total=total+nums[l];
+    if(total>highest){
+        highest=total;
+    }
+    return sum(k,l+1,highest,total);
+}
+
+for(let i=0,j=nums.length-1;i<nums.length;i++,j--){
+    max=sum(i,nums.length-1-j,max,num);
+}
+console.log(max);
---
+++ day10_alter.js
@@ -0,0 +1,10 @@
+const nums=[-2,1,-3,4,-1,2,1,-5,4];
+let [max]=nums
+right=0;
+
+while(right<nums.length){
+    nums[right]+=nums[right-1]>0?nums[right-1]:0;
+    max=Math.max(max,nums[right]);
+    right++;
+}
+console.log(max);
